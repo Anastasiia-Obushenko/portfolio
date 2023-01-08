95d290c17709ddbcdae6122c41da4cda558baf3d
Anastasiia Obushenko <n.obushenko@gmail.com>
2023-01-08 15:42:06 -0500

    portfolio section + responsivity

--- pages/index.js
+++ pages/index.js
@@ -16,7 +16,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className=' bg-white px-10 '>
+      <main className=' bg-white px-10 md:px-20 lg:px-40'>
         <section className=' min-h-screen'>
           <nav className=' py-10 mb-12 flex justify-between'>
             <h1 className=' text-xl font-burtons'>developedbyName</h1>
@@ -30,15 +30,15 @@ export default function Home() {
             </ul>
           </nav>
           <div className='text-center p-10 '>
-            <h2 className='text-5xl py-2 text-teal-600 font-medium'>First_Name Last_Name</h2>
-            <h3 className=' text-2xl py-2'>Developer</h3>
-            <p className=' text-base py-5 leading-8 text-gray-800'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Similique reiciendis aspernatur saepe eveniet fugiat earum? Iusto laboriosam eveniet magnam fugit ea unde cumque molestiae ipsam amet? Eaque sunt ullam hic.</p>
+            <h2 className='text-5xl py-2 text-teal-600 font-medium md:text-6xl'>First_Name Last_Name</h2>
+            <h3 className=' text-2xl py-2 md:text-3xl'>Developer</h3>
+            <p className=' text-base py-5 leading-8 text-gray-800 md:text-xl max-w-lg mx-auto'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Similique reiciendis aspernatur saepe eveniet fugiat earum? Iusto laboriosam eveniet magnam fugit ea unde cumque molestiae ipsam amet? Eaque sunt ullam hic.</p>
           </div>
           <div className='text-5xl flex justify-center gap-16 py-3 text-gray-600'>
             <AiFillLinkedin />
             <AiFillGithub />
           </div>
-          <div className=" relative mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 mt-20 overflow-hidden">
+          <div className=" relative mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 mt-20 overflow-hidden md:h-96 md:w-96">
             <Image src={avatar} fill style={{ objectFit: "cover" }} />
           </div>
         </section>
@@ -48,7 +48,7 @@ export default function Home() {
             <p className='text-base py-2 leading-8 text-gray-800'>Lorem ipsum dolor sit, amet <span className='text-teal-500'>consectetur adipisicing</span> elit. Facere esse dolorem odio odit, repellendus itaque tempora cum consequuntur iusto. Necessitatibus quae ipsa sed veniam praesentium dignissimos laborum tempore voluptas quasi!</p>
             <p className='text-base py-2 leading-8 text-gray-800'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur atque assumenda nulla repellat voluptates, eaque esse aliquam quasi omnis, odit enim necessitatibus quia suscipit reprehenderit, laboriosam porro nemo architecto sit.</p>
           </div>
-          <div className="">
+          <div className=" lg:flex gap-10">
             <div className=" text-center shadow-lg p-10 rounded-xl my-10">
               <Image src={avatar} width={100} height={100} />
               <h3 className=' text-lg font-medium pt-8 pb-2'>Lorem ipsum dolor</h3>
@@ -75,6 +75,30 @@ export default function Home() {
               <p className='text-gray-800 py-1'>A</p>
               <p className='text-gray-800 py-1'>B</p>
               <p className='text-gray-800 py-1'>C</p>
+            </div>
+          </div>
+        </section>
+        <section>
+          <div >
+            <h3 className=' text-3xl py-1'>Portfolio</h3>
+            <p className='text-base py-2 leading-8 text-gray-800'>Lorem ipsum dolor sit, amet <span className='text-teal-500'>consectetur adipisicing</span> elit. Facere esse dolorem odio odit, repellendus itaque tempora cum consequuntur iusto. Necessitatibus quae ipsa sed veniam praesentium dignissimos laborum tempore voluptas quasi!</p>
+            <p className='text-base py-2 leading-8 text-gray-800'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur atque assumenda nulla repellat voluptates, eaque esse aliquam quasi omnis, odit enim necessitatibus quia suscipit reprehenderit, laboriosam porro nemo architecto sit.</p>
+          </div>
+          <div className=" flex flex-col gap-10 py-10 lg:flex-row lg:flex-wrap">
+            <div className=" basis-1/3 flex-1">
+              <Image src={avatar} className=" rounded-lg object-cover" width={"100%"} height={"100%"} responsive />
+            </div>
+            <div className=" basis-1/3 flex-1">
+              <Image src={avatar} className=" rounded-lg object-cover" width={"100%"} height={"100%"} responsive />
+            </div>
+            <div className=" basis-1/3 flex-1">
+              <Image src={avatar} className=" rounded-lg object-cover" width={"100%"} height={"100%"} responsive />
+            </div>
+            <div className=" basis-1/3 flex-1">
+              <Image src={avatar} className=" rounded-lg object-cover" width={"100%"} height={"100%"} responsive />
+            </div>
+            <div className=" basis-1/3 flex-1">
+              <Image src={avatar} className=" rounded-lg object-cover" width={"100%"} height={"100%"} responsive />
             </div>
           </div>
         </section>
